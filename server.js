1a2392744613a9f2033c6b9db3341b923920a2c6
abhinav <abhinavisgreat18@gmail.com>
2024-11-12 10:33:04 +0530

    updated

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-const express = require('express')
-const colors = require('colors')
-const app = express()
-require('dotenv').config()
-const cors = require('cors')
-app.use(cors())
+const express = require('express');
+const colors = require('colors');
+const cors = require('cors');
+require('dotenv').config();
 
+const app = express();
+app.use(cors());
 
+// Define routes
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-app.get("/home", (req, res) => {
-    res.send('Welcome to my home page!')
-})
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`.bgCyan)
-})
+    res.send('Hello World!');
+});
+app.get('/home', (req, res) => {
+    res.send('Welcome to my home page!');
+});
+
+// Use Vercel's dynamic port or default to 3000 for local testing
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`.bgCyan);
+});
